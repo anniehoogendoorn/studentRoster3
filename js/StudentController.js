ac5dab33b83674b458e8dfb75c4cfe61408ebcdf
jschold <jscholdny@msn.com>
2015-09-21 10:20:08 -0700

    add ability to add and delete a student

--- js/StudentController.js
+++
@@ -1,9 +0,0 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-  $scope.students = [
-    { name: "Sam Schmidt"},
-    { name: "Jessica Martin" },
-    { name: "Sandy Smith" },
-    { name: "Ryan Samuels" },
-    { name: "Brentwood Davis"},
-  ]
-})
